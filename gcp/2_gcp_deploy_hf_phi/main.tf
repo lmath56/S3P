ad28cd2d5193d77cb98358c7a790b7486b55d119
Lucas Mathews <lmath56@hotmail.com>
2024-10-25 12:34:30 +0200

    Now successfully deploys a cluster.

--- gcp/2_gcp_deploy_hf_phi/main.tf
+++ gcp/2_gcp_deploy_hf_phi/main.tf
@@ -5,92 +5,108 @@ terraform {
       version = "6.8.0"
         }
     }
-    backend "gcs" { 
-        bucket  = "s3p-tf-state" # Store the Terraform state in pre-created GCS bucket
-        prefix  = "terraform/2_gcp_deploy_hf_phi"
-    }
 }
 
 provider "google" {
-    project = var.project_id
-    region  = var.region
+    project = "optimal-carving-438111-h3"
+    region  = "europe-west3" 
 }
 
-# Create storage bucket for model data https://cloud.google.com/storage/docs/terraform-create-bucket-upload-object
+# Enable required APIs for GKE cluster
+# Set to disable_on_destroy = false to ensure that the API is not disabled when the resource is destroyed as this takes time to remove and readd
+resource "google_project_service" "compute_api" {
+  project = "optimal-carving-438111-h3"
+  service = "compute.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "container_api" {
+  project = "optimal-carving-438111-h3"
+  service = "container.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "iam_api" {
+  project = "optimal-carving-438111-h3"
+  service = "iam.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "storage_api" {
+  project = "optimal-carving-438111-h3"
+  service = "storage.googleapis.com"
+  disable_on_destroy = false
+}
+
+
+# Create a service account for cluster to access Google Storage
+resource "google_service_account" "gke_service_account" {
+  account_id   = "gke-service-account"
+  display_name = "GKE Service Account"
+}
+
+# Create storage bucket for model data 
+# https://cloud.google.com/storage/docs/terraform-create-bucket-upload-object
 resource "google_storage_bucket" "gcs_bucket" {
- name          = "model-data"
- location      = var.location
+ name          = "s3-model-data-oc"
+ location      = "europe-west3" 
  storage_class = "STANDARD" # https://cloud.google.com/storage/docs/storage-classes
  uniform_bucket_level_access = true # https://cloud.google.com/storage/docs/uniform-bucket-level-access
 }
 
-# IAM permission to allow GKE nodes to access Google Storage via FUSE
-resource "google_project_iam_binding" "gke_storage_access" { # Need to check if this is implemented correctly
-  project = var.project_id
+# IAM permission to allow GKE nodes to access Google Storage 
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_iam
+resource "google_project_iam_binding" "gke_storage_access" { 
+  project = "optimal-carving-438111-h3"
   role    = "roles/storage.objectViewer"
 
   members = [
-    "serviceAccount:${google_container_cluster.gke_cluster.node_config.service_account}", # Need to understand this, need to make an account before this?
+    "serviceAccount:${google_service_account.gke_service_account.email}",
   ]
 }
 
+# Create VPC network and subnet for GKE cluster
+resource "google_compute_network" "gke_network" {
+  name                    = "gke-network"
+  auto_create_subnetworks = false
+  depends_on = [google_project_service.compute_api]  # Ensure Compute Engine API is enabled
+}
+
+resource "google_compute_subnetwork" "gke_subnet" {
+  name          = "gke-subnet"
+  ip_cidr_range = "10.0.0.0/16"
+  region        = "europe-west3" 
+  network       = google_compute_network.gke_network.id
+}
+
+
 # Create GKE Autopilot Cluster with GPU-enabled nodes
 resource "google_container_cluster" "gke_cluster" {
-  name     = "gke-autopilot-cluster"
-  location = var.location
-  node_locations = var.node_locations
-  enable_autopilot = true
-  remove_default_node_pool = true 
+  name     = "gke-hf-phi"
+  location = "europe-west3"
+  node_locations = ["europe-west3-a", "europe-west3-b", "europe-west3-c"]
+  enable_autopilot = true # This will mean that Google will manage the nodes
 
-  # Optional: Network configuration (creates default VPC) # Do I want to do this or should I create a custom VPC?
-  network = google_compute_network.gke_network.id
-  subnetwork = google_compute_subnetwork.gke_subnet.id
-}
+  network = google_compute_network.gke_network.name
+  subnetwork = google_compute_subnetwork.gke_subnet.name
 
-
-# https://cloud.google.com/kubernetes-engine/docs/quickstarts/create-cluster-using-terraform
-resource "google_compute_network" "default" {
-  name = "example-network"
-
-  auto_create_subnetworks  = false
-  enable_ula_internal_ipv6 = true # IPV6? Do I want this?
-}
-
-resource "google_compute_subnetwork" "default" {
-  name = "example-subnetwork"
-
-  ip_cidr_range = "10.0.0.0/16"
-  region        = "us-central1"
-
-  stack_type       = "IPV4_IPV6"
-  ipv6_access_type = "INTERNAL" # Change to "EXTERNAL" if creating an external loadbalancer  # IPV6? Do I want this? 
-
-  network = google_compute_network.default.id
-  secondary_ip_range {
-    range_name    = "services-range"
-    ip_cidr_range = "192.168.0.0/24"
+  workload_identity_config {
+    workload_pool = "optimal-carving-438111-h3.svc.id.goog"
   }
-
-  secondary_ip_range { # Do I need this?
-    range_name    = "pod-ranges"
-    ip_cidr_range = "192.168.1.0/24"
-  }
-}
-
-# Create a node pool with GPU accelerators and preemptible instances (?)
-resource "google_container_node_pool" "node_pool" {
-  name       = "node-pool"
-  cluster    = google_container_cluster.autopilot_cluster.name
-  location   = google_container_cluster.autopilot_cluster.location
-  node_count = 1
-
+  
   node_config {
-    preemptible  = true
-    machine_type = "e2-medium" # Need to check these, how to add GPU?
-
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform",
-    ]
+    workload_metadata_config {
+      mode = "GKE_METADATA"
+    }
+  }
+  addons_config {
+    gcs_fuse_csi_driver_config { # Enable GCS FUSE CSI driver https://github.com/GoogleCloudPlatform/gcs-fuse-csi-driver/blob/main/docs/terraform.md
+      enabled = true
+    }   
   }
 }
 
+# Output the cluster name
+output "cluster_name" {
+  value = google_container_cluster.gke_cluster.name
+}
